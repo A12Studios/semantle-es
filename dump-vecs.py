fea2d881434da28b588f7d2fb15c765c29b2f06c
Cristobal Galleguillos Katz <cgallegu@gmail.com>
2022-02-20 22:46:55 -0500

    feat(dump-vecs): add progress indicators
    This commit adds a progress bar to the sqlite loading loop.
    Useful when loading a dataset for the first time in a machine, as it
    gives ETAs and throughput.
    
    It also adds a simple elapsed time indicator when loading the Words2vec
    file.
    
    Testing: ran `dump-vecs.py` against the Google dataset.

--- dump-vecs.py
+++ dump-vecs.py
@@ -1,8 +1,13 @@
+import time
 import gensim.models.keyedvectors as word2vec
 
 import sqlite3
+from tqdm import tqdm
 
+t_word2vec = time.process_time()
+print("loading word2vec file...")
 model = word2vec.KeyedVectors.load_word2vec_format("../GoogleNews-vectors-negative300.bin", binary=True)
+print(f'done in {time.process_time() - t_word2vec} seconds')
 
 con = sqlite3.connect('word2vec.db')
 cur = con.cursor()
@@ -13,10 +18,17 @@ con.commit()
 
 import pdb;pdb.set_trace()
 
+total_words = len(model.vocab)
+t_load = time.process_time()
+t = tqdm(total=total_words)
 for i, word in enumerate(model.vocab):
     if (i % 1111 == 0):
         con.commit()
     vec = model[word].tostring()
     cur.execute("insert into word2vec values(?,?)", (word,vec))
+    t.update()
 
 con.commit()
+t.close()
+t_total = time.process_time() - t_load
+print(f"done in {t_total} secs, {total_words/t_total} words/sec")
